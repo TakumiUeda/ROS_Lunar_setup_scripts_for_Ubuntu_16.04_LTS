f40aa2340b36ffb560c307014a4e29645c5813ef
リイン <reinforce00111@gmail.com>
2018-04-05 03:54:11 +0900

    add readtest

---
+++ git_setup.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt -y remove git
+sudo apt -y install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev autoconf asciidoc xmlto docbook2x make gcc
+
+
+wget https://github.com/git/git/archive/v2.15.1.tar.gz
+tar -zxf v2.15.1.tar.gz
+
+cd git-2.15.1
+make configure
+./configure --prefix=/usr
+
+make all doc info
+sudo make install install-doc install-html install-info
+git --version
---
+++ test.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Hello Would!"
+exit 0
